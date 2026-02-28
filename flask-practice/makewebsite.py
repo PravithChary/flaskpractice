79a0e2a60bd9f73ecc1286adf9a90702eefbf332
Pravith Chary Kammari <kpravithchary2002@gmail.com>
2026-02-28 15:00:39 +0530

    tut4: http methods(get/post) and retrieving form data

--- flask-practice/makewebsite.py
+++ flask-practice/makewebsite.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, redirect, render_template
+from flask import Flask, url_for, redirect, render_template, request
 
 app = Flask(__name__)
 
@@ -32,9 +32,24 @@ def html_page():
 def jinja_page(name):
     return render_template("jinja.html", content=name, names_list = ["Pravith", "Ranjith", "Bobby"])
 
+# Template inheritance -> creating a skeleton in one file and using in others similar to OOPs
 @app.route("/template-inheritance")
 def template_inheritance():
     return render_template("template-inheritance.html")
+
+# HTTP Methods
+# HTTP considers the default method as GET
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        user = request.form["nm"]
+        return redirect(url_for("user", usr=user))
+    else:
+        return render_template("login.html")
+    
+@app.route("/<usr>")
+def user(usr):
+    return f"Hello {usr}!"
 
 if __name__ == "__main__":
     app.run(debug=True) # debug=True makes the app reload automatically when changes are made to the code
